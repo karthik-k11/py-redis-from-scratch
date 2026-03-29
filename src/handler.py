3cd193b3b289536db90785f04de858305133cc4e
Karthik <karthikk1162@gmail.com>
2026-03-29 17:58:21 +0530

    Implemented SET & GET (In memory store)

--- src/handler.py
+++ src/handler.py
@@ -1,3 +1,5 @@
+from store import store
+
 def handle_command(command_parts):
 
     if not command_parts:
@@ -5,7 +7,33 @@ def handle_command(command_parts):
 
     command = command_parts[0].upper()
 
+    ##PING
     if command == "PING":
         return "+PONG\r\n"
+
+    ##SET key value
+    elif command == "SET":
+        if len(command_parts) < 3:
+            return "-ERR wrong number of arguments for 'SET'\r\n"
+
+        key = command_parts[1]
+        value = command_parts[2]
+
+        store[key] = value
+
+        return "+OK\r\n"
+
+    ##GET key
+    elif command == "GET":
+        if len(command_parts) < 2:
+            return "-ERR wrong number of arguments for 'GET'\r\n"
+
+        key = command_parts[1]
+
+        if key in store:
+            value = store[key]
+            return f"${len(value)}\r\n{value}\r\n"   # RESP Bulk String
+
+        return "$-1\r\n"   # NULL (Redis style)
 
     return "-ERR unknown command\r\n"
--- src/server.py
+++ src/server.py
@@ -4,6 +4,25 @@ from constants import HOST, PORT, BUFFER_SIZE
 from handler import handle_command
 from resp_parser import parse_resp
 
+def is_complete_command(data: str) -> bool:
+
+    data = data.replace("\r\n", "\n")
+    lines = [line for line in data.split("\n") if line.strip() != ""]
+
+    if not lines:
+        return False
+
+    if not lines[0].startswith("*"):
+        return True
+
+    try:
+        num_elements = int(lines[0][1:])
+    except:
+        return False
+
+    expected_lines = 1 + (num_elements * 2)
+
+    return len(lines) >= expected_lines
 
 def handle_client(client_socket, address):
     print(f"[NEW CONNECTION] {address} connected.")
@@ -18,13 +37,12 @@ def handle_client(client_socket, address):
                 print(f"[DISCONNECTED] {address}")
                 break
 
-            decoded_data = data.decode("utf-8")
-            buffer += decoded_data
+            buffer += data.decode("utf-8")
 
             print(f"[RAW BUFFER]\n{buffer}")
 
-            ##Only process if we got full command
-            if "\n" not in buffer:
+            #Only process when full command received
+            if not is_complete_command(buffer):
                 continue
 
             command_parts = parse_resp(buffer)
@@ -32,14 +50,17 @@ def handle_client(client_socket, address):
             print(f"[PARSED] {command_parts}")
 
             if not command_parts:
+                buffer = ""
                 continue
 
             response = handle_command(command_parts)
-
             client_socket.sendall(response.encode("utf-8"))
 
-            buffer = ""  
+            buffer = "" 
 
+        except ConnectionResetError:
+            print(f"[CLIENT CLOSED] {address}")
+            break
         except Exception as e:
             print(f"[ERROR] {e}")
             break
