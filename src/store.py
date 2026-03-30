76ca4268ed0320b4511d3dde1d92b3a61fde096b
Karthik <karthikk1162@gmail.com>
2026-03-30 12:46:04 +0530

    Key value implemented (Ex seconds)

--- src/store.py
+++ src/store.py
@@ -1,1 +1,24 @@
-store = {}
+import time
+
+store = {}       
+expiry = {}      
+
+
+def set_key(key, value, ttl=None):
+    store[key] = value
+
+    if ttl is not None:
+        expiry[key] = time.time() + ttl
+    elif key in expiry:
+        del expiry[key]
+
+
+def get_key(key):
+    ##Check expiry
+    if key in expiry:
+        if time.time() > expiry[key]:
+            del store[key]
+            del expiry[key]
+            return None
+
+    return store.get(key)
