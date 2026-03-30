1dfff6a1685f95ce368815bdbd8561fbe6c44357
Karthik <karthikk1162@gmail.com>
2026-03-30 13:24:06 +0530

    Removed all debug prints

--- src/store.py
+++ src/store.py
@@ -5,7 +5,6 @@ expiry = {}
 
 
 def set_key(key, value, ttl=None):
-    print(f"[DEBUG] set_key called: key={key}, value={value}, ttl={ttl}")
     store[key] = value
 
     if ttl is not None:
@@ -17,14 +16,10 @@ def set_key(key, value, ttl=None):
 
 
 def get_key(key):
-    print(f"[DEBUG] get_key called for: {key}")
     if key in expiry:
-        print(f"[DEBUG] expiry[{key}] = {expiry[key]}, current = {time.time()}")
         current_time = time.time()
-        print(f"[DEBUG] Checking expiry: now={current_time}, expiry={expiry[key]}")
 
         if current_time >= expiry[key]:
-            print(f"[DEBUG] Key expired: {key}")
             store.pop(key, None)
             expiry.pop(key, None)
             return None
