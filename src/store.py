465e1a29814f09b56ab9ec2d59c04323948ac04e
Karthik <karthikk1162@gmail.com>
2026-03-31 23:12:04 +0530

    Made it as only one thread allowed at a time

--- src/store.py
+++ src/store.py
@@ -1,41 +1,44 @@
 import time
+import threading
 
 store = {}
 expiry = {}
 
+lock = threading.Lock()
+
 
 def set_key(key, value, ttl=None):
-    store[key] = value
+    with lock:
+        store[key] = value
 
-    if ttl is not None:
-        expiry_time = time.time() + int(ttl)
-        expiry[key] = expiry_time
-        print(f"[DEBUG] Expiry set: {key} expires at {expiry_time}")
-    else:
-        expiry.pop(key, None)
+        if ttl is not None:
+            expiry[key] = time.time() + int(ttl)
+        else:
+            expiry.pop(key, None)
 
 
 def get_key(key):
-    if key in expiry:
-        current_time = time.time()
+    with lock:
+        if key in expiry:
+            if time.time() >= expiry[key]:
+                store.pop(key, None)
+                expiry.pop(key, None)
+                return None
 
-        if current_time >= expiry[key]:
-            store.pop(key, None)
-            expiry.pop(key, None)
-            return None
+        return store.get(key)
 
-    return store.get(key)
 
 def cleanup_expired_keys():
-    current_time = time.time()
+    with lock:
+        current_time = time.time()
 
-    keys_to_delete = []
+        keys_to_delete = []
 
-    for key, exp_time in expiry.items():
-        if current_time >= exp_time:
-            keys_to_delete.append(key)
+        for key, exp_time in expiry.items():
+            if current_time >= exp_time:
+                keys_to_delete.append(key)
 
-    for key in keys_to_delete:
-        print(f"[CLEANUP] Removing expired key: {key}")
-        store.pop(key, None)
-        expiry.pop(key, None)
+        for key in keys_to_delete:
+            print(f"[CLEANUP] Removing expired key: {key}")
+            store.pop(key, None)
+            expiry.pop(key, None)
